27a5fca7c27439cf0ecfad73995662c233de990d
zhangzhaoxuan <1040517659@qq.com>
2019-07-14 11:07:58 +0800

    队列

--- SortTest.c
+++ SortTest.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
+int a[1000];
 int main()
 {
-
+    int n,i,t;
+    for(int i=0;i<1000;i++)//初始化桶
+        a[i]=0;
+   n=10;
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&t);
+        if(a[t]==1)//去重
+            continue;
+        else
+        a[t]=1;
+    }
+    for(int i=0;i<1000;i++)
+    {
+        if(a[i]==1)
+        printf("%d ",i);
+    }
+    return 0;
 }
